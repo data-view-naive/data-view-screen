9a69bc8c81c6bfabfc8aad0bca018e628b102127
chengyu <rensailong259@hellobike.com>
2021-08-20 12:27:09 +0800

    feat: screen container

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <d-icon name="check-circle" style="font-size: 30px;color: red;"></d-icon>
+  <d-container class="container" :options="{width: 3840, height: 2160}">
+    <p>icon</p>
+    <p>
+      <d-icon name="check-circle" style="font-size: 100px;color: red;"></d-icon>
+    </p>
+    <p>loading</p>
+    <p>
+      <d-loading :width="200" :height="200"/>
+    </p>
+  </d-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { DIcon } from 'data-view-naive';
+import { DIcon, DContainer, DLoading } from 'data-view-naive';
 
 export default defineComponent({
   name: 'App',
   components: {
-    DIcon
+    DIcon,
+    DContainer,
+    DLoading
   }
 })
 </script>
@@ -20,5 +31,18 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+}
+html, body {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+<style>
+.container {
+  font-size: 60px;
+  padding: 40px;
 }
 </style>
